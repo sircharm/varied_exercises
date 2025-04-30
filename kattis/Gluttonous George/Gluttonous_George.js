e564a1546e45aa612858bd60ac2da8fde303bb49
Marcelo Bernardes de Oliveira <marcelo.web13@gmail.com>
2025-04-30 18:16:43 -0300

    Create Gluttonous_George.js

---
+++ kattis/Gluttonous George/Gluttonous_George.js
@@ -0,0 +1,23 @@
+//Accepted (100) - 0.04 s
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('line', (line) => {
+    let numbers = line.split(' ').map(Number);
+    if (numbers[0] > numbers[2]){
+        console.log('>');
+    }
+    else if (numbers[0] < numbers[2]){
+        console.log('<');
+    }
+    else{
+        console.log('Goggi svangur!')
+    }
+}).on('close', () => {
+    process.exit(0)
+});
